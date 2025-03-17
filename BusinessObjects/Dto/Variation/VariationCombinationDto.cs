fa715dd2b7662733bd1d3f18856df6556342cfc4
GordonDang <cuongdhtse184437@fpt.edu.vn>
2025-03-17 10:20:39 +0700

    add marketprice to productitem

--- BusinessObjects/Dto/Variation/VariationCombinationDto.cs
+++ BusinessObjects/Dto/Variation/VariationCombinationDto.cs
@@ -4,6 +4,7 @@
     {
         public List<Guid> VariationOptionIds { get; set; } = new List<Guid>();
         public int Price { get; set; }
+        public int MarketPrice { get; set; }
         public int QuantityInStock { get; set; }
         public string ImageUrl { get; set; } = null!;
     }
