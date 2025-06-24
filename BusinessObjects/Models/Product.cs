b09898adebd10ffc3d0f2926dbc9707da214e09e
Nguyen Son <126442464+sonnguyen1305@users.noreply.github.com>
2025-06-24 10:04:06 +0700

    add PurchasePrice for Product

--- BusinessObjects/Models/Product.cs
+++ BusinessObjects/Models/Product.cs
@@ -33,6 +33,8 @@ public partial class Product : BaseEntity
 
     public decimal MarketPrice { get; set; }
 
+    public decimal PurchasePrice { get; set; }
+
     public string KeyActiveIngredients { get; set; }
 
     public string ExpiryDate { get; set; }
