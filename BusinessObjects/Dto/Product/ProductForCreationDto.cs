5b105c36076a33a9a4aac9df09e7a7ebc2fd21af
sonnguyen1305 <kijtei1@gmail.com>
2025-07-16 12:41:03 +0700

    Remove max length validation from product description
    Eliminated the [StringLength(1000)] attribute from the Description property in both ProductForCreationDto and ProductForUpdateDto, allowing product descriptions to exceed 1000 characters.

--- BusinessObjects/Dto/Product/ProductForCreationDto.cs
+++ BusinessObjects/Dto/Product/ProductForCreationDto.cs
@@ -13,8 +13,6 @@ public class ProductForCreationDto
     [Required(ErrorMessage = "Name is required.")]
     [StringLength(200, ErrorMessage = "Name can't exceed 200 characters.")]
     public string Name { get; set; }
-
-    [StringLength(1000, ErrorMessage = "Description can't exceed 1000 characters.")]
     public string Description { get; set; }
 
     [Required(ErrorMessage = "Price is required.")]
